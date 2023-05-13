718d0ebc7be882522c8456945af1c430b32ca01b
juliay.chamomile@gmail.com <juliay.chamomile@gmail.com>
2023-05-13 23:29:20 +0300

    aad js

---
+++ script.js
@@ -0,0 +1,32 @@
+// function exerrcise1(n) {
+
+//     let number = n;
+
+//     while(number >= 1) {
+
+//         console.log(number);
+
+//         number = number - 1;
+
+//     }
+//     console.log('Start');
+// }
+
+
+// function exerrcise1(a, b, c) {
+//     let number = a;
+//     while( a<=b) {
+//         if(number % c === 0) {
+//             console.log(number);
+//         }
+//         number = number + 1;
+//     }
+// }
+
+// function exerrcise1(b) {
+//     for (let i = 1; i <= b; i = i + 2) {
+//         console.log(i);
+//     }
+// }
+
+
