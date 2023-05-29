dae0f7a3745b8c261cf05307f4ba39f6f41a2b7a
juliay.chamomile@gmail.com <juliay.chamomile@gmail.com>
2023-05-29 23:41:33 +0300

    add

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
-export default {
-    subOptimal: function () {
-        // оптимізуйте код нижче таким чином, щоб уникнути дублювання завдяки використання функції reverseString
-        // будь-ласка, вносіть зміни тільки в межах, позначених лініями
-        // !! ВАЖЛИВО: Виклик reverseString здійснюйте за допомогою this: this.reverseString
+// 8.7
+// export default {
+//     subOptimal: function () {
+//         // оптимізуйте код нижче таким чином, щоб уникнути дублювання завдяки використання функції reverseString
+//         // будь-ласка, вносіть зміни тільки в межах, позначених лініями
+//         // !! ВАЖЛИВО: Виклик reverseString здійснюйте за допомогою this: this.reverseString
 
-        const strings = ['abcd', 'karamba', 'racecar'];
+//         const strings = ['abcd', 'karamba', 'racecar'];
 
-        // ========== Початок зони редагування ==============
+//         // ========== Початок зони редагування ==============
 
-        const reversedStrings = [];
+//         const reversedStrings = [];
           
-        strings.forEach(element => reversedStrings.push(this.reverseString(element)))   
+//         strings.forEach(element => reversedStrings.push(this.reverseString(element)))   
         
 
-        return reversedStrings;
+//         return reversedStrings;
 
-        // ========== Кінець зони редагування ===============
-    },
+//         // ========== Кінець зони редагування ===============
+//     },
 
-    reverseString: function (str) {
-        return str.split('').reverse().join('');
+//     reverseString: function (str) {
+//         return str.split('').reverse().join('');
+//     }
+// };
+
+
+
+export const person = {
+    name: 'Petro',
+    age: 35,
+    gender: 'male',
+    education: 'university',
+    occupation: 'developer',
+    isAdmin: true
+  };
+  
+  export const person1 = {
+    name: 'Jane',
+    age: 37,
+    gender: 'female',
+    education: 'college',
+    occupation: 'designer',
+    isAdmin: false
+  };
+  
+  export function processPerson(person) {
+    if (person && typeof person === 'object') {
+      const genders = {
+        male: 'Чоловік',
+        female: 'Жінка'
+      };
+  
+      let summary = 'Інформація про особу: \n';
+  
+      summary += genders[person.gender] || 'Особа невідомої статі';
+      summary += ' ' + person.name;
+      summary += ' віком ' + person.age + ' роки(-ів)';
+      summary += ' має освіту на рівні ' + person.education;
+      summary += ' та працює на посаді ' + person.occupation + '.';
+  
+      if (person.isAdmin) {
+        summary += ' Є адміністратором.';
+      }
+  
+      return summary;
     }
-};
-
-
+  
+    throw new Error("Очікується об'єкт");
+  }
 
